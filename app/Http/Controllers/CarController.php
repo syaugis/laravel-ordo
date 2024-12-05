e46064191450a7c34eb11e842106d1a3dadff690
syaugis <m.syaugishahab@gmail.com>
2024-12-05 15:05:50 +0700

    Eloquent

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,9 +12,8 @@ class CarController extends Controller
 {
     public function index(): View
     {
-        $cars = DB::table('cars')
-            ->whereYear('date_of_manufacture', '<=', 2022)
-            ->where('price', '>', 30000000000)
+        $cars = Car::whereYear('date_of_manufacture', '>', 2022)
+            ->where('type', 'Pickup Truck')
             ->get();
 
         return view('car', compact('cars'));
@@ -21,12 +21,11 @@ class CarController extends Controller
 
     public function store(): RedirectResponse
     {
-        DB::table('cars')->insert([
-            ['name' => 'Tesla CyberTruck', 'type' => 'Pickup Truck', 'price' => 2500000000, 'date_of_manufacture' => '2024-01-01'],
-            ['name' => 'Honda Civic RS', 'type' => 'Sedan', 'price' => 61680000000, 'date_of_manufacture' => '2022-06-01'],
-            ['name' => 'Hyundai i20', 'type' => 'Hatchback', 'price' => 36004000000, 'date_of_manufacture' => '2022-04-04'],
-            ['name' => 'Wrangler Rubicon', 'type' => 'SUV', 'price' => 1730000000, 'date_of_manufacture' => '2024-02-01'],
-            ['name' => 'Toyota Kijang Innova 2.0 G AT', 'type' => 'MPV', 'price' => 30000000000, 'date_of_manufacture' => '2020-06-05'],
+        Car::create([
+            'name' => 'Mitsubishi Supra',
+            'type' => 'MPV',
+            'price' => 35000000000,
+            'date_of_manufacture' => '2018-01-01',
         ]);
 
         return redirect('car')->with('success', 'Cars Have Been Inserted Successfully');
